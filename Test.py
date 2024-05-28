475ff9de039dd667a5e2aabe286ede6185152c70
VuAnh183 <dinhvuanh2708@gmail.com>
2024-05-28 21:50:59 +0700

    Still failed but made some progress

--- Test.py
+++ Test.py
@@ -33,52 +33,82 @@ class TuringMachine:
         self.current_state = init_state
         self.accept_states = accept_states
         self.input_tape = input_tape
-        self.x_tape = [""] # Initialize the x tape with a blank symbol
-        self.y_tape = [""] # Initialize the y tape with a blank symbol
+        self.tape = [""] # Initialize a tape with a blank symbol
         self.head = 0 # Initialize the tape head postion
     
     # Define what the turing machine does
     def step(self):
-      current_state = self.transitions[0][0]
-      input_symbol = self.transitions[0][1]
-      
-      match current_state:
-        case "q0":
-          if(input_symbol == "1" and input_symbol == "0"):
-            self.tape.append(input_symbol)
-            if(move_direction)
-          else:
-            exit()
+      step = 0
+      x_value = 0
+      n = 0
+      print(self.input_tape)
+      while step < len(self.transitions):
+        new_state = self.transitions[step][0]
+        input_symbol = self.transitions[step][1]
+        output_symbol = self.transitions[step][2]
+        move_direction = self.transitions[step][3]
+        next_state = self.transitions[step][4]
         
-        case "q1":
-          if(input_symbol == "1" and input_symbol == "0"):
-            self.tape.append(input_symbol)
+        print(f"Step {step}")
+        print(self.current_state, new_state, self.input_tape[self.head])
+        if(self.current_state == new_state):
+             
+          if(self.input_tape[self.head] == input_symbol):
+            # q0: is the initial state, stop if the initial symbol is #
+            if(self.current_state == "q0"):
+              if(self.input_tape[self.head] == "#"):
+                print("End")
+                break
+              else:
+                self.current_state = next_state
+                step += 1
+                continue
+                  
+            # q1: move to right until the symbol # is found, after that move left to get all symbols of x      
+            if(self.current_state == "q1"):
+              print(self.transitions[step])
+              print("Reach q1! Current_state: " + self.current_state + " Head symbol: " + self.input_tape[self.head])
+              if(self.input_tape[self.head] == "1" or self.input_tape[self.head] == "0"):
+                print(self.input_tape[self.head])
+                print(self.current_state)
+                self.head += 1
+              elif(self.input_tape[self.head] == "#"):
+                current_state = new_state
             
-            
-        case "q2":
-          if(input_symbol == "1" and input_symbol == "0"):
-            self.tape.append(input_symbol)
-          
-          
+          # # q2: move left to convert x to decimal      
+          # if(self.current_state == "q2"):
+          #   print("Reach q1: " + self.current_state)
+          #   n += 1
+          #   if(self.input_tape[self.head] == "1"):
+          #     print("new x value:" + x_value)
+          #     x_value += pow(2, n)
+          #     if(move_direction == "L"):
+          #       if(self.head <= 0):
+          #         current_state = new_state
+          #       self.head -= 1
+                           
+        step += 1
+
+
         
 transitions = [
-  ("q0", "1", "0", "R", "q1"),
-  ("q0", "0", "0", "R", "q1"),
+  ("q0", "1", "0", "H", "q1"),
+  ("q0", "0", "0", "H", "q1"),
   ("q0", "#", "0", "H", "qH"),
   ("q1", "1", "0", "R", "q1"),
   ("q1", "0", "0", "R", "q1"),
   ("q1", "#", "0", "R", "q2"),
   ("q2", "1", "0", "R", "q2"),
-  ("q2", "0", "0", "R", "q2"),
-  ("q2", "#", "0", "R", "q3"), 
-  ("q3", "1", "0", "R", "q3"), 
-  ("q3", "0", "0", "R", "q3"), 
-  ("q3", "#", "0", "R", "q4"),
-  ("q4", "0", "0", "0", "q5"), 
-  ("q5", "0", "0", "0", "q6"), 
-  ("q6", "0", "1", "R", "q6"), 
-  ("q6", "0", "0", "R", "q6"),
-  ("q6", "#", "#", "H", "qH")
+  # ("q2", "0", "0", "R", "q2"),
+  # ("q2", "#", "0", "R", "q3"), 
+  # ("q3", "1", "0", "R", "q3"), 
+  # ("q3", "0", "0", "R", "q3"), 
+  # ("q3", "#", "0", "R", "q4"),
+  # ("q4", "0", "0", "0", "q5"), 
+  # ("q5", "0", "0", "0", "q6"), 
+  # ("q6", "0", "1", "R", "q6"), 
+  # ("q6", "0", "0", "R", "q6"),
+  # ("q6", "#", "#", "H", "qH")
   
 ]
 
